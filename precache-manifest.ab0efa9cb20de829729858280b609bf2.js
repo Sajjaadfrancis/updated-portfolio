28e83a958f70fbfc17d69d0ddc33f1ea394fcfa0
Sajjaad Francis <sajjaadfrancis@gmail.com>
2021-02-09 10:37:37 +0100

    Updates

---
+++ precache-manifest.ab0efa9cb20de829729858280b609bf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9948f32723743f582e37bd8862dfaf70",
+    "url": "/updated-portfolio/index.html"
+  },
+  {
+    "revision": "a869335c170806eb26fd",
+    "url": "/updated-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6097dad4b0048285c987",
+    "url": "/updated-portfolio/static/js/2.40d592a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/updated-portfolio/static/js/2.40d592a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a869335c170806eb26fd",
+    "url": "/updated-portfolio/static/js/main.05be4645.chunk.js"
+  },
+  {
+    "revision": "ec2e7adf51226d93cb7d",
+    "url": "/updated-portfolio/static/js/runtime-main.a10fccb3.js"
+  }
+]);
